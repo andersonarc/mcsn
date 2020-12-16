1d9377f444bedf38cfa97392438c02495bae1b51
andersonarc <e.andersonarc@gmail.com>
2020-12-16 01:00:48 +0000

    development in progress, use submodules

---
+++ src/mcsn/include/main.h
@@ -0,0 +1,15 @@
+/**
+ * @file main.h
+ * @author andersonarc (e.andersonarc@gmail.com)
+ * @brief mcsn entry poin dependencies
+ * @version 0.1
+ * @date 2020-12-14
+ */
+    /* header guard */
+#ifndef MCSN_MAIN_H
+#define MCSN_MAIN_H
+
+    /* includes */
+#include "mcsn.h" /* mcsn functions */
+
+#endif /* MCSN_MAIN_H */
--- src/mcsn/main.c
+++ src/mcsn/main.c
@@ -1,5 +1,20 @@
-#include "protocol.h"
+/**
+ * @file main.c
+ * @author andersonarc (e.andersonarc@gmail.com)
+ * @brief mcsn entry point
+ * @version 0.1
+ * @date 2020-12-14
+ */
+    /* includes */
+#include "main.h" /* this */
 
-int main() {
-    return CLIENTBOUND;
+/**
+ * @brief mcsn entry point and main function
+ * 
+ * @param argc argument count
+ * @param argv arguments
+ * @return int exit status
+ */
+int main(int argc, const char* argv[]) {
+    mcsn_connect(argv[1], 25565);
 }
